4a2b41ed7f003303bf38b60c24441790f23c35f8
Stanislav Teroff <teroff13@gmail.com>
2026-01-27 21:51:20 -0700

    Updated emacs to .emacs.d

---
+++ .config/.emacs.d/init.el
@@ -0,0 +1,15 @@
+;;; init.el --- Personal Emacs configuration
+;;; Stanislav Teroff
+;;; -*- lexical-binding: t; -*-
+
+;; --------------------
+;; UI / Appearance
+;; --------------------
+
+(load-theme 'tango-dark t)
+
+(setq display-line-numbers-type 'relative)
+
+(global-display-line-numbers-mode 1)
+
+(message "Loaded personal Emacs configuration")
